0ba0d3c85f635dda7a020103b561d5c9bbfbd731
Hitakshi Gautam <hitakshi.gautam03@gmail.com>
2026-05-18 20:54:46 +0530

    min in rotated sorted array q 153

---
+++ BinarySearch/q153.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        int ans=Integer.MAX_VALUE;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[low] <= nums[high]) {
+                ans = Math.min(ans, nums[low]);
+                break;
+            }
+            ans=Math.min(ans,nums[mid]);
+            if(nums[low]<=nums[mid]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;   
+    }
+}
