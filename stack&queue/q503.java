d304a082d56a32239dbd4d752d911c5d749c6613
Hitakshi Gautam <hitakshi.gautam03@gmail.com>
2026-06-13 15:23:44 +0530

    next greater element 2 q 503

---
+++ stack&queue/q503.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> stack=new Stack<>();
+        int n=nums.length;
+        int[] nge=new int[n];
+        for(int i = 2*n-1; i >= 0; i--){
+            int idx = i % n;
+            while(!stack.isEmpty() && stack.peek() <= nums[idx]){
+                stack.pop();
+            }
+            if(i < n){
+                nge[idx] = stack.isEmpty() ? -1 : stack.peek();
+            }
+            stack.push(nums[idx]);
+        }
+        return nge;
+    }
+}
